9b71e8b3bb26e02e30449cb7b98dd730f3982fc2
Joyen Benitto <joyen.benitto12@gmail.com>
2023-07-18 20:54:40 +0530

    substrate generation added

--- ant_gen/generator.py
+++ ant_gen/generator.py
@@ -1,5 +1,37 @@
 import template as temp
+import yaml
+import time
 
 def generator(build_dir):
+    ti = time.ctime()
+    data = []
+    with open('test/pass/pass1.yaml') as f:
+        data = yaml.load(f, Loader= yaml.SafeLoader)
+
+    project_name = data['project_name']
+    unit = data['unit']
+    ground_plane_X_size = data['ant']['substrate']['Dimensions'][0]
+    ground_plane_Y_size = data['ant']['substrate']['Dimensions'][1]
+    ground_plane_Z_size = data['ant']['substrate']['Dimensions'][2]
+    ground_offset_x = data['ant']['substrate']['offset'][0]
+    ground_offset_y = data['ant']['substrate']['offset'][1]
+    ground_offset_z = data['ant']['substrate']['offset'][2]
+    material =data['ant']['substrate']['Material']
+
     with open(f"{build_dir}/generated.py","w") as fp:
-        fp.write(temp.template_msp)
+        # Creating the groud  plane and the substrate
+        fp.write(temp.template_msp.format(
+            project = project_name, 
+            time = ti,
+            month = "Jul",
+            day = 12,
+            year = 2023,
+            unit = unit,
+            ground_plane_X_pos = -ground_plane_X_size/2 + ground_offset_x,
+            ground_plane_Y_pos = -ground_plane_Y_size/2 + ground_offset_y,
+            ground_plane_Z_pos =  0,
+            ground_plane_X_size = ground_plane_X_size,
+            ground_plane_Y_size = ground_plane_Y_size,
+            ground_plane_Z_size = ground_plane_Z_size,
+            material = material
+        ))
--- ant_gen/main.py
+++ ant_gen/main.py
@@ -4,9 +4,6 @@ import utils
 import yaml
 
 build_dir = 'build'
-data = []
-with open('test/pass/pass1.yaml') as f:
-    data = yaml.load(f, Loader= yaml.SafeLoader)
 
 utils.clean_dir(build_dir)
 os.system(f"mkdir {build_dir}")
